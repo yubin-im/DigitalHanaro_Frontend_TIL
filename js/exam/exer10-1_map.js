dc63bf5091483b3491251578a7fffae6939f37c2
[yubin-im] <[ybyb130@gmail.com]>
2024-02-01 17:45:24 +0900

    JavaScript: 10 연습문제 풀이(map)

--- js/exam/exer10-1_map.js
+++ js/exam/exer10-1_map.js
@@ -8,15 +8,26 @@ const hong = {id: 1, name: 'Hong', dept: 1};
 const kim = {id: 2, name: 'Kim', dept: 2};
 const emps = [ hong, kim, {id:3, name: 'Park', dept: 2}, {id: 4, name: 'Choi', dept: 2} ];
 
+
 const deptMap = new Map();
 deptMap.set(1, hrTeam);
 deptMap.set(2, devTeam);
 console.log(deptMap); // Map(2) { 1 => { id: 1, dname: '인사팀' }, 2 => { id: 2, dname: '개발팀' } }  ⇐ deptMap.get(2)
 
+// 문제 풀이
+const deptMap_ = new Map(depts.map(dept => [dept.id, dept]));
+console.log('풀이: ', deptMap_);
+
+
 const empMap = new Map();
 empMap.set(1, hong);
 empMap.set(2, kim);
 console.log(empMap); // Map(2) { 1 => {id: 1, name: 'Hong', dept: 1}, 2 => {id: 2, name: 'Kim', dept: 2}, … }
+
+// 문제 풀이
+const empMap_ = new Map(emps.map(emp => [emp.id, emp]));
+console.log('풀이: ', empMap_);
+
 
 const empDept = new Map();
 const { id, name, ...rest } = hong;
@@ -27,6 +38,12 @@ empDept.set(kim, devTeam);
 empDept.set(emps[2], devTeam);
 empDept.set(emps[3], devTeam);
 console.log(empDept); // Map(4) { { id: 1, name: 'Hong' } => { id: 1, dname: '인사팀' }, { id: 2, name: 'Kim' } => { id: 2, dname: '개발팀' }, { id: 3, name: 'Park' } => { id: 2, dname: '개발팀' }, { id: 4, name: 'Choi' } => { id: 2, dname: '개발팀' } }
+
+// 문제 풀이
+const empDept_ = new Map();
+const xx = emps.map(({ id, name, dept }) => [{ id, name }, deptMap.get(dept)]);
+console.log("🚀 ~ xx:", xx)
+
 
 // console.log(empDept.get(kim).dname); // '개발팀'
 // // 개발팀 직원 목록 출력 ⇒ Kim, Park, Choi
