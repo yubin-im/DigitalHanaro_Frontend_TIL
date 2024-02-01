ea0e3e53dadfe4e9ed915b752acafde18db824c2
[yubin-im] <[ybyb130@gmail.com]>
2024-02-01 10:11:06 +0900

    JavaScript: 07 연습문제(array, reduce) 풀이 2

--- js/exam/exer07-4_reduce.js
+++ js/exam/exer07-4_reduce.js
@@ -3,8 +3,11 @@
 // 배열 각 요소의 제곱근   n => Math.sqrt(n)      [sqrt]
 // 배열의 각 요소를 세제곱  n => n ** 3            [cube]
 // 문제 풀이
+const assert = require('assert');
 
 const arr = [1, 2, 3, 4, 5];
+const square = n => n ** 2;
+const cube = n => n ** 3;
 
 const ret3_1 = arr
     .map(a => a ** 2)
@@ -12,3 +15,18 @@ const ret3_1 = arr
     .map(a => a ** 3);
 
 console.log("🚀 ~ ret3_1:", ret3_1)
+assert.deepStrictEqual(ret3_1, [1, 8, 27, 64, 125]);
+
+// square(2) => Math.sqrt(4) => cube
+const ret3_2 = [square, Math.sqrt, cube].reduce((acc, f) => f(acc), 2);
+const bp1 = n => [square, Math.sqrt, cube].reduce((acc, f) => f(acc), n);
+console.log("🚀 ~ ret3_2:", ret3_2, bp1(2));
+
+const ret3_3 = arr.map(a => bp1(a));
+console.log("🚀 ~ ret3_3:", ret3_3);
+assert.deepStrictEqual(ret3_3, [1, 8, 27, 64, 125]);
+
+const bpm= (fns, n) => fns.reduce((acc, f) => f(acc), n);
+
+const ret3_4 = bpm([square, Math.sqrt, cube], 2);
+console.log("🚀 ~ ret3_4:", ret3_4)
