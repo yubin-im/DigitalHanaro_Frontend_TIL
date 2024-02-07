06040f2e28b748f908b3e8dbf5e0e75a1b6690ea
yubin-im <ybyb130@gmail.com>
2024-02-07 14:59:28 +0900

    JavaScript: 14 연습문제 풀이 (비동기 프로그래밍)

--- js/exam/exer14-2.js
+++ js/exam/exer14-2.js
@@ -2,12 +2,46 @@
 // - 1번 유저의 글목록: https://jsonplaceholder.typicode.com/posts?userId=1
 // - 댓글 목록: https://jsonplaceholder.typicode.com/posts/<postId>/comments
 
-getPosts(1);
-[
-  {
-    postId: ,
-    title: ,
-    comments:
-  },
-  {}
-]
+// 문제 풀이 
+
+const userId = 1;
+const res = await fetch(
+  `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+);
+const posts = (await res.json()).map(({ id: postId, title }) => ({
+  postId,
+  title,
+}));
+console.log('🚀  posts:', posts.length);
+
+async function getCommentsAsync(postId) {
+  const cRes = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+  );
+  return cRes.json();
+}
+
+const getCommentsPromi = postId =>
+  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`).then(
+    cRes => cRes.json()
+  );
+
+console.time('comments-serial-request');
+for (const post of posts) {
+  post.comments = await getCommentsAsync(post.postId);
+}
+
+// console.log('🚀  posts:', JSON.stringify(posts, null, '  '));
+
+console.timeEnd('comments-serial-request');
+
+console.time('comments-parelell-request');
+const comments = await Promise.allSettled(
+  posts.map(post => getCommentsPromi(post.postId))
+);
+posts.forEach((post, idx) => {
+  post.comments = comments[idx];
+});
+
+// console.log('🚀  posts:', JSON.stringify(posts, null, '  '));
+console.timeEnd('comments-parelell-request');
