835fe856e4da001ac97bdf22b5634e3d5251d31c
yubin-im <ybyb130@gmail.com>
2024-02-07 12:20:43 +0900

    git 연결 진짜 확인

--- ts/hello.ts
+++ ts/hello.ts
@@ -5,15 +5,8 @@ type User = {
     name: string;
 };
 
-const s: string = 'abc';
+// const s: string = 'abc';
 let i = 1;
 
 const hong: User = {id: 1, name: 'Hong'};
 const kim: User = {id: 2, name: 'Kim'};
-
-interface UserIf {
-    id: number;
-    name: string;
-    city?: string;
-    getName(): string;
-}
