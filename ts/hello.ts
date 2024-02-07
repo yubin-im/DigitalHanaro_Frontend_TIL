5a9ce3b4df91317d7a8cba25b5a8108b2068d43e
[yubin-im] <[ybyb130@gmail.com]>
2024-02-07 11:10:34 +0900

    TypeScript: 16 타입스크립트 시작하기

---
+++ ts/hello.ts
@@ -0,0 +1,12 @@
+console.log('Hello, TypeScript!');
+
+type User = {
+    id: number;
+    name: string;
+};
+
+const s: string = 'abc';
+let i = 1;
+
+const hong: User = {id: 1, name: 'Hong'};
+const kim: User = {id: 2, name: 'Kim'};
