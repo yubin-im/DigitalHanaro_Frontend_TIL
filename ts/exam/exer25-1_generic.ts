5b6fcd6db10a7f52395b98166564f62c1f21fc0e
yubin-im <ybyb130@gmail.com>
2024-02-13 17:45:55 +0900

    TypeScript: 24 연습문제 풀이 (Generic)

---
+++ ts/exam/exer25-1_generic.ts
@@ -0,0 +1,20 @@
+// 특정 key의 타입을 변경하는 Change 유틸리티 타입 만들기
+// 문제 풀이
+interface IUser {
+    id: number;
+    age: number;
+    name: string;
+}
+
+interface IDept {
+    id: number;
+    age: string;
+    dname: string;
+    captain: string;
+}
+
+type Change<T, K extends keyof T, U> = {
+    [k in keyof T]: k extends K ? U : T[k];
+};
+type DeptCaptain = Change<IDept, 'captain', IUser>;
+type Err = Change<IDept, 'somekey', IUser>;  // Error!!!
---
+++ ts/exam/exer25-2_generic.ts
@@ -0,0 +1,18 @@
+// 다음 코드가 오류가 나지 않도록 수정하시오.
+// 단, itemPrices의 item에는 재고(stock)에 있는 item들만 가능합니다.
+// 문제 풀이 
+type Item = { item: string; price: number };
+type ItemPrice<T, U> = {
+    [k in keyof T]: k extends 'item' ? keyof U : T[k]
+};
+
+const stock = { X: 1, Y: 2, Z: 30 };
+
+const itemPrices: ItemPrice<Item, typeof stock>[] = [
+  { item: 'X', price: 1000 },
+  { item: 'Y', price: 2000 },
+  { item: 'Z', price: 3000 },
+];
+
+const total = itemPrices.reduce((curr, itemPrice) => 
+                  curr + stock[itemPrice.item] * itemPrice.price, 0);
