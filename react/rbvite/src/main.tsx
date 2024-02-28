1a9873e06505b390fd9d288c40aa4efd218c4567
yubin-im <ybyb130@gmail.com>
2024-02-28 16:38:21 +0900

    React: github pages 배포

--- react/rbvite/src/main.tsx
+++ react/rbvite/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { CounterProvider } from './contexts/counter-context.tsx';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
